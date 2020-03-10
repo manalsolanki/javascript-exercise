88880e0381e9bc805e9647acd3ab557441c5c294
Manal Upendra Solanki <n01397767@nj-00118-019.humber.org>
2020-03-10 15:25:54 -0400

    Chnages to javascript file

---
+++ js/lab-session.js
@@ -0,0 +1,9 @@
+
+let helloHeading = document.getElementById("hello")
+//update content
+helloHeading.textContent="goodbye"
+//update single css property
+helloHeading.style.color="blue"
+helloHeading.style.backgroundColor="green"
+//update class=""
+helloHeading.className="whatever goodbye"
